b8c2ac5d980608827e0586dce804b2728b1a7ad1
GitHub Action <action@github.com>
2020-12-02 06:20:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/doubleluck/plugindemoi/services/MyProjectService.kt
+++ src/main/kotlin/com/github/doubleluck/plugindemoi/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.doubleluck.plugindemoi.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.doubleluck.plugindemoi.MyBundle
 
 class MyProjectService(project: Project) {
 
